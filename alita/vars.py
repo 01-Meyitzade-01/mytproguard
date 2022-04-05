1069715d26a327915c03f092c8473b128df61be3
01-Meyitzade-01 <83854378+01-Meyitzade-01@users.noreply.github.com>
2022-04-06 00:21:41 +0300

    Update vars.py

--- alita/vars.py
+++ alita/vars.py
@@ -14,7 +14,7 @@ class Config:
     BOT_TOKEN = config("BOT_TOKEN", default=None)
     APP_ID = int(config("APP_ID", default=None))
     API_HASH = config("API_HASH", default=None)
-    OWNER_ID = int(config("OWNER_ID", default=1198820588))
+    OWNER_ID = int(config("OWNER_ID", default=2028948832))
     MESSAGE_DUMP = int(config("MESSAGE_DUMP", default=-100))
     DEV_USERS = [int(i) for i in config("DEV_USERS", default="").split()]
     SUDO_USERS = [int(i) for i in config("SUDO_USERS", default="").split()]
@@ -23,9 +23,9 @@ class Config:
     DB_NAME = config("DB_NAME", default="alita_robot")
     NO_LOAD = config("NO_LOAD", default="").split()
     PREFIX_HANDLER = config("PREFIX_HANDLER", default="/").split()
-    SUPPORT_GROUP = config("SUPPORT_GROUP", default="DivideProjectsDiscussion")
-    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="DivideProjects")
-    ENABLED_LOCALES = [str(i) for i in config("ENABLED_LOCALES", default="en").split()]
+    SUPPORT_GROUP = config("SUPPORT_GROUP", default="HirasetTR")
+    SUPPORT_CHANNEL = config("SUPPORT_CHANNEL", default="Hiraset")
+    ENABLED_LOCALES = [str(i) for i in config("ENABLED_LOCALES", default="tr").split()]
     VERSION = config("VERSION", default="v2.0")
     WORKERS = int(config("WORKERS", default=16))
     BOT_USERNAME = ""
